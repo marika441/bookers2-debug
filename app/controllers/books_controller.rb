81683822c69918e38cd010ad988c3c3815580263
EC2 Default User <s.marika441@gmail.com>
2021-11-27 11:53:06 +0000

    [Fix]books/showエラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,13 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @newbook = Book.new
   end
 
   def index
+    @user = current_user
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -34,16 +37,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
